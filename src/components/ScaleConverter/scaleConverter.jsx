f5f6c53531a2e54ecb3a63531f1c656fda6231ac
afroz <afrozdd7@gmail.com>
2025-02-18 18:16:26 +0530

    added time units and conversion values

--- src/components/ScaleConverter/scaleConverter.jsx
+++ src/components/ScaleConverter/scaleConverter.jsx
@@ -97,9 +97,14 @@ function ScaleConverter ({ toggleView, handleClick, display }) {
             },
         },
         Time: {
-            units: [],
+            units: [ " Milliseconds","Seconds", "Minutes", "Hours", "Days", "Weeks"],
             conversion: {
-
+                Milliseconds: {  Seconds: 0.001, Minutes: 0.000016667, Hours: 2.777, Days: 1.15740, Weeks: 1.653439  },     
+                Seconds: { Milliseconds: 1000,  Minutes: 0.0166666667, Hours: 0.002777778, Days: 0.0000115741, Weeks: 0.0000016534 },
+                Minutes: { Milliseconds: 60000, Seconds: 60,  Hours: 0.0166, Days: 0.0006944, Weeks: 0.0000992063 },
+                Hours: { Milliseconds: 3600000, Seconds: 3600, Minutes: 60,  Days: 0.041, Weeks: 0.0059 },
+                Days: { Milliseconds: 86400000, Seconds: 86400, Minutes: 1440, Hours: 24,  Weeks: 0.1428571429 },
+                Weeks: { Milliseconds: 604800000, Seconds: 604800, Minutes: 10080, Hours: 168, Days: 7},
             },
         },
         Tip: {
