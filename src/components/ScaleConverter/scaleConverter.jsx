51dd6ede362f488066d917b5664e4b5170caf8f6
afroz <afrozdd7@gmail.com>
2025-03-01 18:32:48 +0530

    initiated up and down arrow buttons logic.

--- src/components/ScaleConverter/scaleConverter.jsx
+++ src/components/ScaleConverter/scaleConverter.jsx
@@ -13,6 +13,14 @@ function ScaleConverter ({ toggleView, handleClick, display, handleClear, handle
     const [currentCategory, setCurrentCategory] = useState("Length");
     const [fromUnit, setFromUnit] = useState("Inches");
     const [toUnit, setToUnit] = useState("Centimetres");
+    const [cursorPosition, setCursorPosition] = useState("from");
+
+    const handleArrowKeys = (direction) => {
+        if (direction === "up" || direction === "down") {
+            setCursorPosition(cursorPosition === "from" ? "to" : "from");
+        }
+    };
+
 
 
     // Units and conversion factors
@@ -23,12 +31,12 @@ function ScaleConverter ({ toggleView, handleClick, display, handleClear, handle
                 Inches: { Centimetres: 2.54,  Millimetres: 25.4, Metres: 0.0254, Kilometres: 0.000025, Feet: 0.0833, Yards: 0.0278, Miles: 0.000015, Mils: 1000  },
                 Centimetres: { Inches: 0.3937, Millimetres: 10, Metres: 0.01, Feet: 0.0328, Yards: 0.0109, Kilometres: 0.00001, Miles: 0.00000062, Mils: 393.7 },
                 Metres: { Inches: 39.37, Millimetres: 1000,  Feet: 3.28, Yards: 1.09, Centimetres: 100, Kilometres: 0.001, Miles: 0.0006, Mils: 39370.07 },
-                Millimetres : {Inches: 0.0393, Centimetres: 0.1, Meters: 0.001, Kilometres: 0.000001, Feet: 0.0032, Yards: 0.0010, Miles: 6.213,  Mils: 39.37},
-                Kilometers: { Centimetres: 1000000, Inches: 39370.07 , Millimetres: 1000000 , Metres: 1000 ,  Feet: 3280.83, Yards: 1093.61 , Miles: 0.62 , Mils: 39370078.74 },
-                Feet: { Centimetres: 30.48, Inches: 12, Millimetres: 304.8, Metres: 0.304, Kilometers: 0.0003, Yards: 0.333, Miles: 0.00018, Mils: 12000  },
-                Yards: { Centimetres: 91.44, Inches: 36, Millimetres: 914.4, Metres: 0.914, Kilometers: 0.00091, Feet: 3,  Miles: 0.00056, Mils: 36000 },
-                Miles: { Centimetres: 160934.4, Inches: 63360, Millimetres: 1609344, Metres: 1609.34, Kilometers: 1.609, Feet: 5280, Yards: 1760,  Mils: 63360000 },
-                Mils: { Centimetres: 0.00254, Inches: 0.001, Millimetres: 0.0254, Metres: 0.000025, Kilometers: 2.540, Feet: 0.000083, Yards: 0.000027, Miles: 1.578 },
+                Millimetres : {Inches: 0.0393, Centimetres: 0.1, Metres: 0.001, Kilometres: 0.000001, Feet: 0.0032, Yards: 0.0010, Miles: 6.213,  Mils: 39.37},
+                Kilometres: { Centimetres: 1000000, Inches: 39370.07 , Millimetres: 1000000 , Metres: 1000 ,  Feet: 3280.83, Yards: 1093.61 , Miles: 0.62 , Mils: 39370078.74 },
+                Feet: { Centimetres: 30.48, Inches: 12, Millimetres: 304.8, Metres: 0.304, Kilometres: 0.0003, Yards: 0.333, Miles: 0.00018, Mils: 12000  },
+                Yards: { Centimetres: 91.44, Inches: 36, Millimetres: 914.4, Metres: 0.914, Kilometres: 0.00091, Feet: 3,  Miles: 0.00056, Mils: 36000 },
+                Miles: { Centimetres: 160934.4, Inches: 63360, Millimetres: 1609344, Metres: 1609.34, Kilometres: 1.609, Feet: 5280, Yards: 1760,  Mils: 63360000 },
+                Mils: { Centimetres: 0.00254, Inches: 0.001, Millimetres: 0.0254, Metres: 0.000025, Kilometres: 2.540, Feet: 0.000083, Yards: 0.000027, Miles: 1.578 },
             },
         },
         Area: {
@@ -43,7 +51,7 @@ function ScaleConverter ({ toggleView, handleClick, display, handleClear, handle
                 SquareInches: { Ares: 0.0000064, SquareMetres: 0.00064, SquareCentimetres: 6.4516, Acres: 1.594, Hectares: 6.451, SquareFeet: 0.006 }, 
             },
         },
-        Temparature: {
+        Temperature: {
             units: ["Fahrenheit", "Celsius" , "Kelvin"],
             conversion: {
                 Fahrenheit: { Celsius: -17.2222, Kelvin: 255.92},
@@ -132,6 +140,9 @@ function ScaleConverter ({ toggleView, handleClick, display, handleClear, handle
         return (numValue * conversionFactor).toFixed(2);
     };
 
+    
+
+
 
 
 
@@ -176,9 +187,8 @@ function ScaleConverter ({ toggleView, handleClick, display, handleClear, handle
                                 </option>
                             ))}
                         </select>
-                        {/* if the display is empty, it falls back to the value of "1" */}
-                        <span className='ip-value'>
-                            {display || "1"} {fromUnit.slice(0, 2).toLowerCase()}               
+                        <span className={`ip-value ${cursorPosition === "from" ? "blink-cursor" : ""}`}>
+                            {display || "1"} {fromUnit.slice(0, 2).toLowerCase()}
                         </span>
                     </div>
                     <div className="to-unit">
@@ -192,7 +202,7 @@ function ScaleConverter ({ toggleView, handleClick, display, handleClear, handle
                                 </option>
                             ))}
                         </select>
-                        <span className='op-value'>
+                        <span className={`op-value ${cursorPosition === "to" ? "blink-cursor" : ""}`}>
                             {performConversion(display || "1")} {toUnit.slice(0, 2).toLowerCase()}
                         </span>
                     </div>
@@ -204,7 +214,7 @@ function ScaleConverter ({ toggleView, handleClick, display, handleClear, handle
       
 
             </div>
-            <Keypad handleClick={handleClick} handleClear={handleClear} handleDelete={handleDelete}/>
+            <Keypad handleClick={handleClick} handleClear={handleClear} handleDelete={handleDelete} handleArrowKeys={handleArrowKeys}/>
         </>
     );
 }
